e23ca8ecfedc8a25cfd02d12820d207dc8987f83
fede <federico.pedretti@buonarroti.tn.it>
2025-12-01 14:42:38 +0100

    Aggiunti js e css in to-do-list

--- toDoList/script.js
+++ toDoList/script.js
@@ -1,4 +1,38 @@
-const li = document.createElement('li'); //Creaiamo un elemento lista 
-li.appendChild(span); 
-li.appendChild(button); 
-li.removeChild('li');
+//Acquisiamo in input 
+const input = document.getElementById("task");
+
+//Bottone aggiungi 
+const btnAdd = document.getElementById("add-btn");
+
+const lista = document.getElementById("listaTasks");
+
+//Al click del bottone chiamo aggiungi 
+btnAdd.onclick = aggiungi; //Si può anche fare in html onclick="aggiungi()"
+
+function aggiungi() {
+    const testo = input.value.trim(); //trim toglie gli spazi in eccesso (quelli esterni)
+    if (testo === "") {
+        alert("Devi inserire qualcosa nella text box se vuoi aggiungere un task!");
+    }
+    else {
+        //Creo elemento di lista 
+        const elemento = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = testo + " ";
+        const btnElimina = document.createElement("button");
+        btnElimina.textContent = "Elimina";
+
+        //Se il bottone btnElimina viene cliccato elimino l'elemento 
+        btnElimina.onclick = function () {
+            lista.removeChild(elemento);
+        };
+
+        //Aggiungere lo span e il bottone alla lista 
+        elemento.appendChild(span);
+        elemento.appendChild(btnElimina);
+
+        lista.appendChild(elemento);
+
+        input.value = null;
+    }
+}
