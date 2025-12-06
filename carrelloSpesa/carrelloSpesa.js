3db37d146102004ca1a63584f97cd1af1447d828
federico <federico.pedretti@buonarroti.tn.it>
2025-12-06 19:33:56 +0100

    Modificato js carrello spesa

--- carrelloSpesa/carrelloSpesa.js
+++ carrelloSpesa/carrelloSpesa.js
@@ -3,36 +3,45 @@ const tabella = document.getElementById("table-carrello");
 function aggiungiProdotto(testo, prezzo){
     const riga = document.createElement("tr"); 
     const tdElemento = document.createElement("td"); 
-    const btnTogli = document.createElement("td"); 
+    const btnTogli = document.createElement("button"); 
     const tdQuantità = document.createElement("td"); 
-    const btnAggiungi = document.createElement("td"); 
+    const btnAggiungi = document.createElement("button"); 
     const tdPrezzo = document.createElement("td");
+    const btnElimina = document.createElement("button"); 
     
     tdElemento.textContent = testo; 
-    tdQuantità.type = number; 
-    tdQuantità.step = 1; 
     tdQuantità.textContent = 1; 
-    tdPrezzo = (tdQuantità.value)*prezzo; 
+    tdPrezzo.textContent = prezzo + "€"; 
     btnAggiungi.textContent = "+"; 
     btnTogli.textContent = "-"; 
+    btnElimina.textContent = "ELIMINA"; 
 
-    btnAggiungi = aumentaQuantità; 
-    btnTogli = diminuisciQuantità; 
+    btnAggiungi.onclick = () => aumentaQuantità(tdQuantità, tdPrezzo, prezzo);
+    btnTogli.onclick = () => diminuisciQuantità(tdQuantità, tdPrezzo, prezzo);
 
     riga.appendChild(tdElemento); 
     riga.appendChild(btnTogli); 
     riga.appendChild(tdQuantità); 
     riga.appendChild(btnAggiungi); 
     riga.appendChild(tdPrezzo); 
+    riga.appendChild(btnElimina); 
+
+    btnElimina.onclick = () => riga.parentNode.removeChild(riga);
     
     tabella.appendChild(riga); 
 }
 
-function aumentaQuantità(){
-    tdQuantità += 1; 
-    tdPrezzo = (tdQuantità.value)*prezzo; 
+function aumentaQuantità(tdQuantità, tdPrezzo, prezzo){
+    let quantità = parseInt(tdQuantità.textContent); 
+    quantità +=1; 
+    tdQuantità.textContent = quantità; 
+    tdPrezzo.textContent = (quantità)*prezzo + "€"; 
 }
-function diminuisciQuantità(){
-    tdQuantità -= 1; 
-    tdPrezzo = (tdQuantità.value)*prezzo; 
+function diminuisciQuantità(tdQuantità, tdPrezzo, prezzo) {
+    let quantità = parseInt(tdQuantità.textContent);
+    if( quantità>1 ){
+        quantità -= 1;
+        tdQuantità.textContent = quantità;
+        tdPrezzo.textContent = (quantità) * prezzo + "€";
+    }
 }
