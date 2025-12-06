a304b929ad1d30711499ef7594082d2102e15aef
federico <federico.pedretti@buonarroti.tn.it>
2025-12-06 22:01:05 +0100

    Finito progetto carrello spesa

--- carrelloSpesa/carrelloSpesa.js
+++ carrelloSpesa/carrelloSpesa.js
@@ -1,4 +1,6 @@
 const tabella = document.getElementById("table-carrello"); 
+const totSpesa = document.getElementById("totale-spesa"); 
+let tot = 0; 
 
 function aggiungiProdotto(testo, prezzo){
     const riga = document.createElement("tr"); 
@@ -15,6 +17,8 @@ function aggiungiProdotto(testo, prezzo){
     btnAggiungi.textContent = "+"; 
     btnTogli.textContent = "-"; 
     btnElimina.textContent = "ELIMINA"; 
+    tot += prezzo; 
+    totSpesa.textContent = "Totale spesa: " + tot + "€"; 
 
     btnAggiungi.onclick = () => aumentaQuantità(tdQuantità, tdPrezzo, prezzo);
     btnTogli.onclick = () => diminuisciQuantità(tdQuantità, tdPrezzo, prezzo);
@@ -26,7 +30,7 @@ function aggiungiProdotto(testo, prezzo){
     riga.appendChild(tdPrezzo); 
     riga.appendChild(btnElimina); 
 
-    btnElimina.onclick = () => riga.parentNode.removeChild(riga);
+    btnElimina.onclick = () => rimuoviProdotto(riga, prezzo, tdQuantità.textContent);
     
     tabella.appendChild(riga); 
 }
@@ -35,6 +39,8 @@ function aumentaQuantità(tdQuantità, tdPrezzo, prezzo){
     let quantità = parseInt(tdQuantità.textContent); 
     quantità +=1; 
     tdQuantità.textContent = quantità; 
+    tot += prezzo; 
+    totSpesa.textContent = "Totale spesa: " + tot + "€"; 
     tdPrezzo.textContent = (quantità)*prezzo + "€"; 
 }
 function diminuisciQuantità(tdQuantità, tdPrezzo, prezzo) {
@@ -42,6 +48,14 @@ function diminuisciQuantità(tdQuantità, tdPrezzo, prezzo) {
     if( quantità>1 ){
         quantità -= 1;
         tdQuantità.textContent = quantità;
+        tot -= prezzo; 
+        totSpesa.textContent = "Totale spesa: " + tot + "€"; 
         tdPrezzo.textContent = (quantità) * prezzo + "€";
     }
 }
+
+function rimuoviProdotto(riga, prezzo, quantità){
+    tot -= prezzo * parseInt(quantità); 
+    riga.parentNode.removeChild(riga); 
+    totSpesa.textContent = "Totale spesa: " + tot + "€"; 
+};
